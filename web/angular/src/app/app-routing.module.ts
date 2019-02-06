367a81ac764807a2dfb5d205f234af2b4833edee
simon987 <fortier.simon@protonmail.com>
2019-02-05 21:44:15 -0500

    translated projects page & title

--- web/angular/src/app/app-routing.module.ts
+++ web/angular/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {NavigationEnd, NavigationStart, Router, RouterModule, Routes} from '@angular/router';
 import {LogsComponent} from "./logs/logs.component";
 import {ProjectDashboardComponent} from "./project-dashboard/project-dashboard.component";
 import {ProjectListComponent} from "./project-list/project-list.component";
 import {CreateProjectComponent} from "./create-project/create-project.component";
 import {UpdateProjectComponent} from "./update-project/update-project.component";
+import {Title} from "@angular/platform-browser";
+import {filter} from "rxjs/operators";
+import {TranslateService} from "@ngx-translate/core";
 
 const routes: Routes = [
     {path: "log", component: LogsComponent},
@@ -19,4 +22,22 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {
+
+    constructor(private title: Title, private router: Router, private translate: TranslateService) {
+        router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe((event: NavigationStart) => {
+                    this.updateTitle(translate, title, event.url)
+                }
+            );
+
+        translate.onLangChange.subscribe(() =>
+            this.updateTitle(translate, title, router.url)
+        )
+    }
+
+    private updateTitle(tr: TranslateService, title: Title, url: string) {
+        tr.get("title." + url.slice(1)).subscribe((t) => title.setTitle(t))
+    }
 }
+
