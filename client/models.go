9cb8891024edb2f5f8123ef6d3b5282dc40c74ae
simon987 <me@simon987.net>
2020-06-29 17:38:36 -0400

    update assignTaskResponse in client

--- client/models.go
+++ client/models.go
@@ -21,19 +21,7 @@ type AssignTaskResponse struct {
 	Message        string  `json:"message"`
 	RateLimitDelay float64 `json:"rate_limit_delay,omitempty"`
 	Content        struct {
-		Task *struct {
-			Id                int64              `json:"id"`
-			Priority          int16              `json:"priority"`
-			Project           *storage.Project   `json:"project"`
-			Assignee          int64              `json:"assignee"`
-			Retries           int16              `json:"retries"`
-			MaxRetries        int64              `json:"max_retries"`
-			Status            storage.TaskStatus `json:"status"`
-			Recipe            string             `json:"recipe"`
-			MaxAssignTime     int64              `json:"max_assign_time"`
-			AssignTime        int64              `json:"assign_time"`
-			VerificationCount int16              `json:"verification_count"`
-		} `json:"task"`
+		storage.Task `json:"task"`
 	} `json:"content"`
 }
 
